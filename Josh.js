10612f09f8dae906688c4217b1da848050784b78
jlc-vcg <Josh.Loan-Clarke@outlook.com>
2022-11-02 07:07:15 +0000

    Completed all formatting and additions except adding actual content.

--- Josh.js
+++ Josh.js
@@ -1,34 +1,32 @@
-//Modified from: https://pagedart.com/blog/how-to-add-a-search-bar-in-html/
-const f = document.getElementById('science-search');
-const q = document.getElementById('query');
-
-function submitted(event, q) {
-  event.preventDefault();
-  if (q == /[Pp]hysic?/) {
-    return physics
-  }
-}
-
-f.addEventListener('submit', submitted);
-
-
-
-
-
-  
-/*IF INPUT == "Chemistry" THEN
-  return chemistry
-IF INPUT == "Biology" THEN
-  return biology
-# Use Regex about for case insensitivity in first letter
-IF INPUT == "Biology" THEN
-  return biology
-# Semi joke entry for CS: "It has been widely accepted that computers have had no impact or effects on computer science. ;-)"
-# Below code modified from W3Schools
-try {
-    if(x == "") throw "That's blank.";
-    if(x == REGEX(*[0-9]*)) throw "Please don't include numbers.";
+function updateParagraph(userInput) {
+  //Regex not working.
+  //const rePhys = /[Pp]hysics?/;
+  //const reChem = new RegExp(/[Cc]hemistry/);
+  //const reBio = new RegExp(/[Bb]iology/);
+  //const reMath = new RegExp(/[Mm]ath(s?|(ematics)?)/);
+  //Below try...catch code modified from W3Schools.
+  try {
+    //Should improve this with switch...case.
+    if (userInput == "") {
+      throw "That's blank.";
+    }
+    if (userInput == "physics" | userInput == "Physics") {
+      scienceParagraph.data = "ScienceText/physics.html";
+    }
+    else if (userInput == "chemistry" | userInput == "Chemistry") {
+      scienceParagraph.data = "ScienceText/chemistry.html";
+    }
+    else if (userInput == "biology" | userInput == "Biology") {
+      scienceParagraph.data = "ScienceText/biology.html";
+    }
+    else if (userInput == "computing" | userInput == "Computing"| userInput == "computer science"| userInput == "Computer Science") {
+      scienceParagraph.data = "ScienceText/computing.html";
+    }
+    else {
+      alert(userInput)
+    }
   }
   catch(err) {
-    message.innerHTML = err;
-  }*/
+    alert(err);
+  }
+}
